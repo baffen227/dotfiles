910c14bf43af547be48882b8b6683ea3feaf89e6
Harry Chen <baffen227@gmail.com>
2025-04-08 00:47:00 +0800

    Move vim to packages/editors.nix

--- echoes/etc/nixos/configuration.nix
+++ echoes/etc/nixos/configuration.nix
@@ -1,3 +1,4 @@
+
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
@@ -6,8 +7,12 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # List packages installed in system profile. To search, run:
+      # $ nix search wget
       ./packages
+
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
@@ -100,7 +105,6 @@
     git
     stow
     tree
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
   ];
 
