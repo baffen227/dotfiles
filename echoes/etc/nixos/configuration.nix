c5e88ba1230ffe3a20aa6a8a1e59f5f62a49816f
Harry Chen <baffen227@gmail.com>
2025-04-10 16:09:49 +0800

    Add input method

--- echoes/etc/nixos/configuration.nix
+++ echoes/etc/nixos/configuration.nix
@@ -66,18 +66,29 @@
   time.timeZone = "Asia/Taipei";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "zh_TW.UTF-8";
-    LC_IDENTIFICATION = "zh_TW.UTF-8";
-    LC_MEASUREMENT = "zh_TW.UTF-8";
-    LC_MONETARY = "zh_TW.UTF-8";
-    LC_NAME = "zh_TW.UTF-8";
-    LC_NUMERIC = "zh_TW.UTF-8";
-    LC_PAPER = "zh_TW.UTF-8";
-    LC_TELEPHONE = "zh_TW.UTF-8";
-    LC_TIME = "zh_TW.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "zh_TW.UTF-8";
+      LC_IDENTIFICATION = "zh_TW.UTF-8";
+      LC_MEASUREMENT = "zh_TW.UTF-8";
+      LC_MONETARY = "zh_TW.UTF-8";
+      LC_NAME = "zh_TW.UTF-8";
+      LC_NUMERIC = "zh_TW.UTF-8";
+      LC_PAPER = "zh_TW.UTF-8";
+      LC_TELEPHONE = "zh_TW.UTF-8";
+      LC_TIME = "zh_TW.UTF-8";
+    };
+
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5.addons = with pkgs; [
+        fcitx5-chewing
+        fcitx5-gtk
+      ];
+    };
   };
 
   # Enable the X11 windowing system.
